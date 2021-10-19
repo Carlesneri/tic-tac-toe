bf1ca3f0c1590b40220f49ab4b11bd9ddda4a2c0
Carlesneri <carlesneri@hotmail.com>
2021-10-19 18:08:52 +0200

    fix: change env vars for deploy

--- api/config.js
+++ api/config.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const PORT = 3001
-export const DB_URI = process.env.DB_URI
-export const SECRET = process.env.SECRET
+export const DB_URI = process.env.TIC_TAC_TOE_DB_URI
+export const SECRET = process.env.TIC_TAC_TOE_SECRET
 export const COOKIE_SESSION_NAME = 'tic-tac-toe.sid'
