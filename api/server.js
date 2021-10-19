8f0317bf6a730d7cc27d9ade3f86f1c7ed4373a8
Carlesneri <carlesneri@hotmail.com>
2021-10-20 01:00:37 +0200

    fix: removing unnecessary code

--- api/server.js
+++ api/server.js
@@ -1,17 +1,17 @@
 import express from 'express'
 import gameRoutes from './routes/game.js'
-import cors from 'cors'
-import session from 'express-session'
-import { COOKIE_SESSION_NAME, SECRET } from './config.js'
+// import cors from 'cors'
+// import session from 'express-session'
+// import { COOKIE_SESSION_NAME, SECRET } from './config.js'
 import cookieParser from 'cookie-parser'
 
 const app = express()
 
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://nerione.xyz'],
-  methods: 'GET, POST, DELETE',
-  credentials: true
-}))
+// app.use(cors({
+  // origin: ['http://localhost:3000', 'https://nerione.xyz'],
+  // methods: 'GET, POST, DELETE',
+  // credentials: true
+// }))
 
 app.use(cookieParser())
 
